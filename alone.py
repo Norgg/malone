ad983bc791093c7116c2e95eff7150daf2eec2f7
John Turner <j.turner@mhnltd.co.uk>
2011-12-17 17:12:19 +0000

    Player controls.

--- alone.py
+++ alone.py
@@ -7,48 +7,75 @@ import os
 
 import cherrypy
 
+from cherrypy.process import plugins
+
 from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
-from ws4py.server.handler.threadedhandler import WebSocketHandler, EchoWebSocketHandler
+from ws4py.server.handler.threadedhandler import WebSocketHandler
 
 world = World()
 
+KEYDOWN = 1
+KEYUP = 2
+MOUSEDOWN = 3
+
+class WorldRunner(plugins.SimplePlugin):
+  def stop(self):
+    world.stop()
+
 class AloneWebSocketHandler(WebSocketHandler):
-    def received_message(self, m):
-        print "Got: %s" % m
-        cherrypy.engine.publish('websocket-broadcast', str(m))
+  def __init__(self, sock, protocols, extensions):
+    print "Player joined."
+    WebSocketHandler.__init__(self, sock, protocols, extensions)
+    world.add_player(self)
+    if not world.is_alive():
+      world.start()
+
+  def received_message(self, m):
+    print "Got: %s" % list(m.data)
+    
+    if (m.data[0] == KEYDOWN):
+      world.keydown(self, m.data[1])
+
+    #self.send(m.data, binary=True)
+    #cherrypy.engine.publish('websocket-broadcast', str(m))
+
+  #def closed(self, code, reason=None):
+  #  print "Closed."
+  #  world.del_player(self)
 
 class Root(object):
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
+  def __init__(self, host, port):
+    self.host = host
+    self.port = port
 
-    @cherrypy.expose
-    def ws(self):
-        cherrypy.log("Handler created: %s" % repr(cherrypy.request.ws_handler))
+  @cherrypy.expose
+  def ws(self):
+    cherrypy.log("Handler created: %s" % repr(cherrypy.request.ws_handler))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Echo CherryPy Server')
-    parser.add_argument('--host', default='0.0.0.0')
-    parser.add_argument('-p', '--port', default=9000, type=int)
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser(description='Malone Server')
+  parser.add_argument('--host', default='0.0.0.0')
+  parser.add_argument('-p', '--port', default=9000, type=int)
+  args = parser.parse_args()
 
-    cherrypy.config.update({'server.socket_host': args.host,
-                            'server.socket_port': args.port,
-                            'tools.staticdir.root': os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))})
+  cherrypy.config.update({'server.socket_host': args.host,
+                          'server.socket_port': args.port,
+                          'tools.staticdir.root': os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))})
 
-    WebSocketPlugin(cherrypy.engine).subscribe()
-    cherrypy.tools.websocket = WebSocketTool()
+  WebSocketPlugin(cherrypy.engine).subscribe()
+  WorldRunner(cherrypy.engine).subscribe()
+  cherrypy.tools.websocket = WebSocketTool()
 
-    cherrypy.quickstart(Root(args.host, args.port), '', config={
-        '/ws': {
-            'tools.websocket.on': True,
-            'tools.websocket.handler_cls': AloneWebSocketHandler
-            },
-        '/': {
-              'tools.staticdir.on': True,
-              'tools.staticdir.dir': '',
-              'tools.staticdir.index': 'index.html'
-            }
-        }
-    )
+  cherrypy.quickstart(Root(args.host, args.port), '', config={
+    '/ws': {
+      'tools.websocket.on': True,
+      'tools.websocket.handler_cls': AloneWebSocketHandler
+    },
+    '/': {
+      'tools.staticdir.on': True,
+      'tools.staticdir.dir': '',
+      'tools.staticdir.index': 'index.html'
+    }
+  }
+)
 
--- world.py
+++ world.py
@@ -1,10 +1,81 @@
 from Box2D import *
+from threading import Thread
+from random import random
+import time
 
-class World(object):
+KEY_W = 119
+KEY_S = 115
+KEY_A = 97
+KEY_D = 100
+
+class World(b2World, Thread):
   def __init__(self):
     aabb = b2AABB()
     aabb.lowerBound = (-100, -100)
     aabb.upperBound = ( 100,  100)
-    gravity = b2Vec2(0, -10)
-    self.world = b2World(aabb, gravity, True)
+    gravity = b2Vec2(0, 0)
+    self.players = {}
+    b2World.__init__(self, aabb, gravity, True)
+    Thread.__init__(self)
+    self.running = True
+    self.tick = 0
 
+  def add_player(self, conn):
+    self.players[conn] = Player(self, conn)
+    print("Added player to world.")
+
+  def del_player(self, conn):
+    del self.players[conn]
+    print("Removed player.")
+
+  def keydown(self, conn, key):
+    player = self.players[conn]
+    
+    if (key == KEY_W):
+      player.body.ApplyForce((0, 0.1), (0, 0))
+    elif (key == KEY_S):
+      player.body.ApplyForce((0, -0.1), (0, 0))
+    elif (key == KEY_A):
+      player.body.ApplyForce((-0.1, 0), (0, 0))
+    elif (key == KEY_D):
+      player.body.ApplyForce((0.1, 0), (0, 0))
+
+  def run(self):
+    print("World started")
+    while(self.running):
+      time.sleep(0.015)
+      self.Step(1.0/60, 10, 10)
+      self.tick += 1
+
+      for player in self.players.values():
+        #player.body.ApplyForce((0.1, 0), (0, 0))
+        if self.tick % 100 == 0:
+          print player.body.position
+        pass
+      
+  def stop(self):
+    print("World Stopped")
+    self.running = False
+
+
+class Player(object):
+  def __init__(self, world, conn):
+    health = 100
+    self.world = world
+    self.conn = conn
+
+    #Create physics shape
+    bodyDef = b2BodyDef()
+    bodyDef.position = (random(), random())
+    self.body = world.CreateBody(bodyDef)
+    shape = b2CircleDef()
+    shape.pos=(0,0)
+    shape.radius = 0.5
+    shape.density = 1
+    self.body.CreateShape(shape)
+    self.body.SetMassFromShapes()
+
+  def damage(d):
+    health -= d
+    if (health < 0):
+      self.world.del_player(self.conn)
